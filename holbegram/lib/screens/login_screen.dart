a6775f6c92759064c60b2cf5e3a32aeef3e190eb
cableyesto <dialkins@tutamail.com>
2026-04-22 19:37:34 +0200

    feat: done ta task 3. Signup Page and 4. Linking the Pages.

--- holbegram/lib/screens/login_screen.dart
+++ holbegram/lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/text_field.dart';
+import 'signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   final TextEditingController emailController;
@@ -125,7 +126,20 @@ class _LoginScreenState extends State<LoginScreen> {
                       children: [
                         const Text("Don't have an account"),
                         TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SignUp(
+                                  emailController: TextEditingController(),
+                                  usernameController: TextEditingController(),
+                                  passwordController: TextEditingController(),
+                                  passwordConfirmController:
+                                      TextEditingController(),
+                                ),
+                              ),
+                            );
+                          },
                           child: const Text(
                             'Sign up',
                             style: TextStyle(
---
+++ holbegram/lib/screens/signup_screen.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import '../widgets/text_field.dart';
+import 'login_screen.dart';
+
+class SignUp extends StatefulWidget {
+  final TextEditingController emailController;
+  final TextEditingController usernameController;
+  final TextEditingController passwordController;
+  final TextEditingController passwordConfirmController;
+
+  const SignUp({
+    super.key,
+    required this.emailController,
+    required this.usernameController,
+    required this.passwordController,
+    required this.passwordConfirmController,
+  });
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  bool _passwordVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = true;
+  }
+
+  @override
+  void dispose() {
+    widget.emailController.dispose();
+    widget.usernameController.dispose();
+    widget.passwordController.dispose();
+    widget.passwordConfirmController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 28),
+            const Text(
+              'Holbegram',
+              style: TextStyle(
+                fontFamily: 'Billabong',
+                fontSize: 50,
+              ),
+            ),
+            Image.asset(
+              'assets/images/logo.webp',
+              width: 80,
+              height: 60,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'Sign up to see photos and videos from your friends.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  const SizedBox(height: 28),
+                  TextFieldInput(
+                    controller: widget.emailController,
+                    ispassword: false,
+                    hintText: 'Email',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: widget.usernameController,
+                    ispassword: false,
+                    hintText: 'Full Name',
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: widget.passwordController,
+                    ispassword: !_passwordVisible,
+                    hintText: 'Password',
+                    keyboardType: TextInputType.visiblePassword,
+                    suffixIcon: IconButton(
+                      color: const Color.fromARGB(218, 226, 37, 24),
+                      alignment: Alignment.bottomLeft,
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  TextFieldInput(
+                    controller: widget.passwordConfirmController,
+                    ispassword: !_passwordVisible,
+                    hintText: 'Confirm Password',
+                    keyboardType: TextInputType.visiblePassword,
+                    suffixIcon: IconButton(
+                      color: const Color.fromARGB(218, 226, 37, 24),
+                      alignment: Alignment.bottomLeft,
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 28),
+                  SizedBox(
+                    height: 48,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(
+                          const Color.fromARGB(218, 226, 37, 24),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  const Divider(thickness: 2),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text('Have an account? '),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginScreen(
+                                  emailController: TextEditingController(),
+                                  passwordController: TextEditingController(),
+                                ),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            'Log in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(218, 226, 37, 24),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
