31235fa469367cccfe47a65eaa57b7b9a958e486
cableyesto <dialkins@tutamail.com>
2026-04-22 23:01:52 +0200

    chore: remove print debug section.

--- holbegram/lib/methods/auth_methods.dart
+++ holbegram/lib/methods/auth_methods.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:http/http.dart' as http;
 import '../models/user.dart';
 
 class AuthMethode {
@@ -39,12 +38,10 @@ class AuthMethode {
     Uint8List? file,
   }) async {
     if (email.isEmpty || password.isEmpty || username.isEmpty) {
-      print('SignUp Error: Please fill all the fields');
       return 'Please fill all the fields';
     }
 
     try {
-      print('Creating user with email: $email');
       UserCredential userCredential =
           await _auth.createUserWithEmailAndPassword(
         email: email,
@@ -52,7 +49,6 @@ class AuthMethode {
       );
 
       User? user = userCredential.user;
-      print('User created with UID: ${user?.uid}');
 
       Users users = Users(
         uid: user!.uid,
@@ -67,7 +63,6 @@ class AuthMethode {
         searchKey: username[0].toUpperCase(),
       );
 
-      print('Saving user to Firestore...');
       //await _firestore.collection("users").doc(user.uid).set(users.toJson());
       await _firestore
           .collection("users")
@@ -79,7 +74,6 @@ class AuthMethode {
               throw Exception('Firestore write timeout - check network connection');
             },
           );
-      print('User saved successfully!');
 
       return 'success';
     } catch (e) {
